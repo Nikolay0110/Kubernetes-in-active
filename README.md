20faf96cf96fd653395b401a22adc5837ea50a9d
nikolay <koluanya0@gmail.com>
2026-01-28 11:13:56 +0300

    fix app.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Kubernetes в действии - Марко Лукша
 Этой мой проект по изучению Kubernetes основанный на книге указанной выше.
+
+1. Сборка образа из Dockerfile
+```bash
+docker build -t kubia .
+```
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const os = require('os');
 
-console.log('Kubia server starting...');
+console.log('Kubia сервер запущен...');
 
-let handler = function(require, response) {
+let handler = function(request, response) {
     console.log('Received request from ' + request.connection.remoteAddress);
     response.writeHead(200);
-    response.end("You've hit " + os.hostname() + "\n");
+    response.end("Вы попали на хост - " + os.hostname() + "\n");
 };
 
 let www = http.createServer(handler);
